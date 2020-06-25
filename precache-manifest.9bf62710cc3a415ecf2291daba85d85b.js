8c345920339341700c0fe8e955fef7978779db77
prpradeep <v-prpradeep@expedia.com>
2020-06-25 15:45:45 +0530

    Updates

---
+++ precache-manifest.9bf62710cc3a415ecf2291daba85d85b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "501e0ce56fb639c0d4caf5744ea3143f",
+    "url": "/publish-react/index.html"
+  },
+  {
+    "revision": "3be1dfa5585d1467c451",
+    "url": "/publish-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2aa4ddb3f02455901f26",
+    "url": "/publish-react/static/js/2.0928ce5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/publish-react/static/js/2.0928ce5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be1dfa5585d1467c451",
+    "url": "/publish-react/static/js/main.3046b892.chunk.js"
+  },
+  {
+    "revision": "cbb8427ca8cc0d7dc7d7",
+    "url": "/publish-react/static/js/runtime-main.558bee71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/publish-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
